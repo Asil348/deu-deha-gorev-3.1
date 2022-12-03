ed7adfcad1a26e1f82ec8c985464a5bcea400da9
Asil <asilefeakyol@gmail.com>
2022-12-03 22:39:54 +0300

    password change functionality

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,15 +1,48 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Profile = () => {
   const { user, setUser }: any = useContext(UserContext);
 
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   const navigate = useNavigate();
 
   const logout = () => {
-    setUser({ username: "", password: "" });
+    setUser({ id: "", username: "", email: "", password: "" });
+    navigate("/");
+  };
+
+  const changePassword = (e: any) => {
+    e.preventDefault();
+
+    if (oldPassword === newPassword) {
+      setOldPassword("");
+      setNewPassword("");
+      setErrorMessage("new password must be different from old password");
+    } else if (oldPassword !== user.password) {
+      setOldPassword("");
+      setNewPassword("");
+      setErrorMessage("Incorrect old password");
+    } else {
+      axios
+        .post("http://localhost:1337/change-password", {
+          id: user.id,
+          password: newPassword,
+        })
+        .then((res) => {
+          console.log(res);
+        });
+      setUser({ ...user, password: newPassword });
+      setOldPassword("");
+      setNewPassword("");
+      setErrorMessage("success");
+    }
   };
 
   useEffect(() => {
@@ -17,7 +50,7 @@ const Profile = () => {
       console.log("not logged in, redirecting to login...");
       navigate("/");
     }
-  }, [])
+  }, []);
 
   return (
     <>
@@ -29,6 +62,23 @@ const Profile = () => {
           <>
             <h1>Username: {user.username}</h1>
             <h1>Email: {user.email}</h1>
+            <form onSubmit={changePassword}>
+              <label>Change Password </label>
+              <input
+                type="password"
+                onChange={(e) => setOldPassword(e.target.value)}
+                value={oldPassword}
+                placeholder="Old password"
+              />
+              <input
+                type="password"
+                onChange={(e) => setNewPassword(e.target.value)}
+                value={newPassword}
+                placeholder="New password"
+              />
+              <input type="submit" value="Değiştir" />
+              <p>{errorMessage}</p>
+            </form>
             <input type="button" value="Logout" onClick={logout} />
           </>
         ) : (
